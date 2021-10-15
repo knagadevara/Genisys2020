4979bf528833fdd16c033de369d5cd3739b416f9
knagadevara <vnsk.1991@gmail.com>
2021-10-15 17:04:03 +0000

    ideal variable usage

--- GEN.V2/ec2-instance/ec2.tf
+++ GEN.V2/ec2-instance/ec2.tf
@@ -9,27 +9,27 @@ locals {
 resource "aws_instance" "Xapp_WEB_PRD"{
     ami=var.AMI_ID
     instance_type=var.Xapp_ENVIRONMENT_TYPE=="PRD" ? var.PRD_Xapp_SHAPE : "t3.micro"
-    count = var.Xapp_ENVIRONMENT_TYPE=="PRD" ? var.PRD_Xapp_SET : 0
+    count = var.Xapp_ENVIRONMENT_TYPE=="PRD" ? var.PRD_Xapp_SET : 4
     tags = local.tags
 }
 
 resource "aws_instance" "Xapp_WEB_STG" {
     ami=var.AMI_ID
     instance_type=var.Xapp_ENVIRONMENT_TYPE=="STG" ? var.STG_Xapp_SHAPE : "t3.micro"
-    count = var.Xapp_ENVIRONMENT_TYPE=="STG" ? var.STG_Xapp_SET : 0
+    count = var.Xapp_ENVIRONMENT_TYPE=="STG" ? var.STG_Xapp_SET : 2
     tags = local.tags
 }
 
 resource "aws_instance" "Xapp_WEB_TST" {
     ami=var.AMI_ID
     instance_type=var.Xapp_ENVIRONMENT_TYPE=="TST" ? var.TST_Xapp_SHAPE : "t3.micro"
-    count = var.Xapp_ENVIRONMENT_TYPE=="TST" ? var.TST_Xapp_SET : 0
+    count = var.Xapp_ENVIRONMENT_TYPE=="TST" ? var.TST_Xapp_SET : 2
     tags = local.tags
 }
 
 resource "aws_instance" "Xapp_WEB_DEV" {
     ami=var.AMI_ID
     instance_type=var.Xapp_ENVIRONMENT_TYPE=="DEV" ? var.DEV_Xapp_SHAPE : "t3.micro"
-    count = var.Xapp_ENVIRONMENT_TYPE=="DEV" ? var.DEV_Xapp_SET : 0 
+    count = var.Xapp_ENVIRONMENT_TYPE=="DEV" ? var.DEV_Xapp_SET : 1 
     tags = local.tags
 }
