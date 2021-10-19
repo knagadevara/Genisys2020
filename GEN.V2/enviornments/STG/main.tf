2e19eb45376daf88da6bab50eda0e2d793c588e0
knagadevara <vnsk.1991@gmail.com>
2021-10-19 07:00:56 +0000

    mod

---
+++ GEN.V2/enviornments/STG/main.tf
@@ -0,0 +1,25 @@
+
+module "Xapp-VPC" {
+  source = "../../modules/vpc"
+}
+
+
+module "Xapp-IAM" {
+  source = "../../modules/iam"
+}
+
+
+module "Xapp-SG" {
+  source = "../../modules/secgrp"
+}
+
+
+module "Xapp-S3" {
+  source = "../../modules/storage_s3"
+}
+
+module "Xapp-EC2-WEBServer" {
+  source = "../../modules/ec2"
+  Xapp_SHAPE = "c5.large"
+  Xapp_SET = "4"
+}
---
+++ GEN.V2/main.tf
@@ -0,0 +1,18 @@
+module "DEV" {
+  source = "./enviornments/DEV"
+}
+
+
+module "TST" {
+  source = "./enviornments/TST"
+}
+
+
+module "STG" {
+  source = "./enviornments/STG"
+}
+
+
+module "PRD" {
+  source = "./enviornments/PRD"
+}
