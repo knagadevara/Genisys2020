1edb93e24791e78664027c3c4eb11315c49ed3c8
knagadevara <vnsk.1991@gmail.com>
2021-10-19 11:13:56 +0000

    main rule

--- GEN.V2/main.tf
+++ GEN.V2/main.tf
@@ -1,19 +1,20 @@
+## This file shall contain only one env pointer at any given time!
 
 module "XAAP-WEB-DEPLOY-DEV" {
   source = "./enviornments/DEV"
 }
 
 
-module "XAAP-WEB-DEPLOY-PRD" {
-  source = "./enviornments/PRD"
-}
+# module "XAAP-WEB-DEPLOY-PRD" {
+#   source = "./enviornments/PRD"
+# }
 
 
-module "XAAP-WEB-DEPLOY-STG" {
-  source = "./enviornments/STG"
-}
+# module "XAAP-WEB-DEPLOY-STG" {
+#   source = "./enviornments/STG"
+# }
 
 
-module "XAAP-WEB-DEPLOY-TST" {
-  source = "./enviornments/TST"
-}
+# module "XAAP-WEB-DEPLOY-TST" {
+#   source = "./enviornments/TST"
+# }
